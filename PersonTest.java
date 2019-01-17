1c8d032010d12637c328ba1634a62979b8c617dd
varunto <35158484+varunto@users.noreply.github.com>
2019-01-17 09:28:57 -0500

    Add files via upload
    Activity 5
    Part 2 (PASS)

--- PersonTest.java
+++ PersonTest.java
@@ -34,10 +34,19 @@ public class PersonTest {
 		 {
 			 String temp = "";
 			 
-			 for(int i=0; i < a.length-2; i++)
+			 for(int i=0; i < a.length-1; i++)
 		        {
-		            
-		          temp = temp +", " + a[i];
+				 
+				 if (i == 0) 
+       		  {
+       	          temp = temp + a[i];
+       		  }
+				 else
+				 {
+					 temp = temp +", " + a[i];
+				 }
+         
+		          
 
 			    }
 			 
